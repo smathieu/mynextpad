76cc9d1fb4eeb160c471b3f592da4fcfcc842ffd
smathieu <simon.math@gmail.com>
2011-05-06 22:19:01 -0400

    Center on montreal

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,25 @@
 $(function() {
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-  
-    var map_options = {
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      zoom: 8,
-      center: latlng
-    };
-  
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+
+  var map_options = {
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 12,
+    center: latlng
+  };
+
   var map = new google.maps.Map(document.getElementById("map_canvas"), map_options)
+  var geocoder = new google.maps.Geocoder();
+
+  function codeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+  codeAddress('Montreal, QC');
+
 });
