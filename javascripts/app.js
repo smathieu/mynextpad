03b0a1e7110d89a876de3f92d17438f3f3684aab
smathieu <simon.math@gmail.com>
2011-05-07 10:38:13 -0400

    Clear markers after each search.

--- javascripts/app.js
+++ javascripts/app.js
@@ -10,21 +10,38 @@ $(function() {
   var map = new google.maps.Map(document.getElementById("map_canvas"), map_options)
   var geocoder = new google.maps.Geocoder();
 
-  var marker = null;
-  function codeAddress(address) {
-    if (marker) {
+  var markers = [];
+
+  function resetMarkers() {
+    $.each(markers, function(i, marker) {
       marker.setMap(null);
-    }
+    });
+  };
+
+  function codeAddress(address) {
+    resetMarkers();
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
-        marker = new google.maps.Marker({
+        var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
         });
+        markers.push(marker);
 
         foursquare.getGroceryStoresNear(marker.getPosition().lat(), marker.getPosition().lng(), function(items) {
           console.log(items);
+          for (var i = 0; i < 5; i++) {
+            var item = items[i];
+            var loc = item.location;
+            var latlng = new google.maps.LatLng(loc.lat, loc.lng);
+            var grocery_marker = marker = new google.maps.Marker({
+                map: map,
+                position: latlng,
+                title: item.name
+            });
+            markers.push(grocery_marker);
+          };
         });
         
       } else {
